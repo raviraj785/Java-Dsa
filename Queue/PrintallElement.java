f2d216dc9b4a42cf7be0d9735760294d211d383f
raviraj785 <ravirajkumarsingh40@gmail.com>
2026-08-24 01:06:05 +0530

    Added Queue

---
+++ Queue/PrintallElement.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PrintallElement {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        // rear -> 5 4 3 2 1 ->front
+        Queue<Integer> helper = new LinkedList<>();
+        while (q.size() > 0) {
+            System.out.print(q.peek() + " ");
+            helper.add(q.poll());
+        }
+        while (helper.size() > 0) {
+            q.add(helper.poll());
+        }
+        System.out.print(q);
+
+    }
+}
---
+++ Queue/basicSTL.java
@@ -0,0 +1,25 @@
+// import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class basicSTL{
+    public static void main(String[] args) {
+        // Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.size());
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+        q.poll();
+        System.out.println(q);
+        System.out.println(q.element());
+        System.out.println(q.peek());
+        System.out.println(q.size());
+
+    }
+}
